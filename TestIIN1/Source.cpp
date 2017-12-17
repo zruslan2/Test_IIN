5fd86f7cb47f2235c4ca7ebd818245f1d1ef77cb
zruslan2@yandex.ru <zruslan2@yandex.ru>
2017-12-17 22:40:31 +0600

    .

--- TestIIN1/Source.cpp
+++ TestIIN1/Source.cpp
@@ -1,10 +1,12 @@
 #include "IIN.h"
 #include <iostream>
+#include <locale.h>
 
 using namespace std;
 
 void main()
 {
+	setlocale(LC_ALL, "Rus");
 	char iin[13];
 	cin >> iin;
 	IIN::IINToData(iin);
