2bb415fae886b44643c078f06a60643661753eba
zruslan2@yandex.ru <zruslan2@yandex.ru>
2017-12-17 22:55:03 +0600

    .

--- TestIIN1/Source.cpp
+++ TestIIN1/Source.cpp
@@ -1,13 +1,26 @@
 #include "IIN.h"
 #include <iostream>
 #include <locale.h>
+#include <windows.h>
 
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	char iin[13];
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char iin[13], den[3], mes[3], god[5], pol[8];
+	cout << "Введите день своего рождения \n";
+	cin >> den;
+	cout << "Введите месяц рождения \n";
+	cin >> mes;
+	cout << "Введите год рождения \n";
+	cin >> god;
+	cout << "Ваш пол \n";
+	cin >> pol;
+	/*cout << "Введите ИИН \n";
 	cin >> iin;
-	IIN::IINToData(iin);
+	IIN::IINToData(iin);*/
+	IIN::DataToIIN(den, mes, god, pol);
 }
